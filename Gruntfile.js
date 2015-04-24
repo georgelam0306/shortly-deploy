e5dc267f57551319125700030d0cfbde8c1deecf
Hack Reactor Students <accounts@hackreactor.com>
2015-04-24 16:41:16 -0700

    Add build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -108,8 +108,7 @@ module.exports = function(grunt) {
     'mochaTest'
   ]);
 
-  grunt.registerTask('build', [
-  ]);
+  grunt.registerTask('build', ['jshint', 'concat', 'uglify', 'mochaTest']);
 
   grunt.registerTask('upload', function(n) {
     if(grunt.option('prod')) {
